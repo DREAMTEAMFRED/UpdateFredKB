c1006928297710585d5c7738a1b962d2eb12602c
Mark Ogilo <44981320+codescream@users.noreply.github.com>
2019-01-24 02:07:51 -0500

    Add project files.

---
+++ UpdateKB/Program.cs
@@ -0,0 +1,205 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UpdateKB
+{
+    class Program
+    {
+        static string host = "https://westus.api.cognitive.microsoft.com";
+        static string service = "/qnamaker/v4.0";
+        static string method = "/knowledgebases/";
+        static string answer = "";
+        static string question = "";
+        // NOTE: Replace this with a valid subscription key.
+        static string key = "209c1cf5f94f4392b1049da8abc4f59b";
+
+        // NOTE: Replace this with a valid knowledge base ID.
+        static string kb = "85e578a7-bf44-4f12-b46e-26efa40b1653";
+
+        static string new_kb = "";
+
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        const int VK_RETURN = 0x0D;
+        const int WM_KEYDOWN = 0x100;
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Enter your question");
+            question = Console.ReadLine();
+            Console.WriteLine("Enter answer to question");
+            answer = Console.ReadLine();
+
+            new_kb = @"
+                        {
+                            'add': {
+                            'qnaList': [
+                                {
+                                'id': 1,
+                                'answer': '" + answer + @"',
+                                'source': 'Custom Editorial',
+                                'questions': [
+                                    '" + question + @"'
+                                ],
+                                'metadata': []
+                                }
+                            ],
+                            'delete': {
+                            'ids': [
+                                0
+                            ]
+                            }
+                        }
+                        ";
+
+            UpdateKB(kb, new_kb);
+            Console.ReadLine();
+            PublishKB();
+            Console.ReadLine();
+        }
+
+        public struct Response
+        {
+            public HttpResponseHeaders headers;
+            public string response;
+
+            public Response(HttpResponseHeaders headers, string response)
+            {
+                this.headers = headers;
+                this.response = response;
+            }
+        }
+
+        static string PrettyPrint(string s)
+        {
+            return JsonConvert.SerializeObject(JsonConvert.DeserializeObject(s), Formatting.Indented);
+        }
+
+        async static Task<Response> Patch(string uri, string body)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = new HttpMethod("PATCH");
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return new Response(response.Headers, responseBody);
+            }
+        }
+
+        async static Task<Response> Get(string uri)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Get;
+                request.RequestUri = new Uri(uri);
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return new Response(response.Headers, responseBody);
+            }
+        }
+
+        async static Task<Response> PostUpdateKB(string kb, string new_kb)
+        {
+            string uri = host + service + method + kb;
+            Console.WriteLine("Calling " + uri + ".");
+            return await Patch(uri, new_kb);
+        }
+
+        async static Task<Response> GetStatus(string operation)
+        {
+            string uri = host + service + operation;
+            Console.WriteLine("Calling " + uri + ".");
+            return await Get(uri);
+        }
+
+        async static void UpdateKB(string kb, string new_kb)
+        {
+            var response = await PostUpdateKB(kb, new_kb);
+            var operation = response.headers.GetValues("Location").First();
+            Console.WriteLine(PrettyPrint(response.response));
+
+            var done = false;
+            while (true != done)
+            {
+                response = await GetStatus(operation);
+                Console.WriteLine(PrettyPrint(response.response));
+
+                var fields = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.response);
+
+                String state = fields["operationState"];
+                if (state.CompareTo("Running") == 0 || state.CompareTo("NotStarted") == 0)
+                {
+                    var wait = response.headers.GetValues("Retry-After").First();
+                    Console.WriteLine("Waiting " + wait + " seconds...");
+                    Thread.Sleep(Int32.Parse(wait) * 1000);
+                }
+                else
+                {
+                    done = true;
+                    CancelConsoleRead();
+                }
+            }
+        }
+
+        async static void PublishKB()
+        {
+            var uri = host + service + method + kb;
+            Console.WriteLine("Calling " + uri + ".");
+            var response = await Post(uri);
+            Console.WriteLine(PrettyPrint(response));
+            CancelConsoleRead();
+            //Console.WriteLine("Press any key to continue.");
+        }
+
+        async static Task<string> Post(string uri)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "{'result' : 'Success.'}";
+                }
+                else
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        static void CancelConsoleRead()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Thread.Sleep(1000);
+
+                var hWnd = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+                PostMessage(hWnd, WM_KEYDOWN, VK_RETURN, 0);
+            });
+        }
+    }
+}
